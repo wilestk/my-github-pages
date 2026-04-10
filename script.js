c1ec5ac4a3b893b689a28f9342d2b520b1023cd0
wilestk <158099750+wilestk@users.noreply.github.com>
2026-04-10 07:40:28 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("testButton");
-
-    button.addEventListener("click", () => {
-        alert("It works!");
-    });
-});
