32ab5b3c1acce25379deb2d719f249eb3ecbb76f
wilestk <158099750+wilestk@users.noreply.github.com>
2026-04-10 04:32:18 -0400

    Add event listener to show alert on button click

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("testButton");
+
+    button.addEventListener("click", () => {
+        alert("It works!");
+    });
+});
